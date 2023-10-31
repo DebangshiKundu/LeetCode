3de8259934395d5958af4b821573daeeaedf7f1d
DebangshiKundu <133627265+DebangshiKundu@users.noreply.github.com>
2023-10-31 20:20:38 +0530

    Time: 16 ms (19.02%), Space: 13.7 MB (58.44%) - LeetHub

--- 0073-set-matrix-zeroes/0073-set-matrix-zeroes.cpp
+++ 0073-set-matrix-zeroes/0073-set-matrix-zeroes.cpp
@@ -1,31 +1,92 @@
 class Solution {
 public:
     void setZeroes(vector<vector<int>>& matrix) {
-        int m=matrix.size();
-        int n=matrix[0].size(),i=0,j=0,a=0,b=0;
-        vector<vector<int>> visit(m, vector<int>(n, 0)); 
-        for(i=0;i<m;i++)
-        {
-            for(j=0;j<n;j++)
-            visit[i][j]=matrix[i][j];
+        // int m=matrix.size();
+        // int n=matrix[0].size(),i=0,j=0,a=0,b=0;
+        // vector<vector<int>> visit(m, vector<int>(n, 0)); 
+        // for(i=0;i<m;i++)
+        // {
+        //     for(j=0;j<n;j++)
+        //     visit[i][j]=matrix[i][j];
+        // }
+        // for(i=0;i<m;i++)
+        // {
+        //     for(j=0;j<n;j++)
+        //     {
+        //         if(matrix[i][j]==0)
+        //         {
+        //             for(a=0;a<m;a++)
+        //                 visit[a][j]=0;
+        //             for(b=0;b<n;b++)
+        //                 visit[i][b]=0;
+        //         }
+        //     }
+        // }
+        // for(i=0;i<m;i++)
+        // {
+        //     for(j=0;j<n;j++)
+        //     matrix[i][j]=visit[i][j];
+        // }
+        int m = matrix.size();
+        int n = matrix[0].size();
+        bool firstRowHasZero = false;
+        bool firstColHasZero = false;
+
+        // Check if the first row has a zero
+        for (int j = 0; j < n; j++) {
+            if (matrix[0][j] == 0) {
+                firstRowHasZero = true;
+                break;
+            }
         }
-        for(i=0;i<m;i++)
-        {
-            for(j=0;j<n;j++)
-            {
-                if(matrix[i][j]==0)
-                {
-                    for(a=0;a<m;a++)
-                        visit[a][j]=0;
-                    for(b=0;b<n;b++)
-                        visit[i][b]=0;
+
+        // Check if the first column has a zero
+        for (int i = 0; i < m; i++) {
+            if (matrix[i][0] == 0) {
+                firstColHasZero = true;
+                break;
+            }
+        }
+
+        // Mark zeros on first row and first column
+        for (int i = 1; i < m; i++) {
+            for (int j = 1; j < n; j++) {
+                if (matrix[i][j] == 0) {
+                    matrix[i][0] = 0;
+                    matrix[0][j] = 0;
                 }
             }
         }
-        for(i=0;i<m;i++)
-        {
-            for(j=0;j<n;j++)
-            matrix[i][j]=visit[i][j];
+
+        // Set rows to zero
+        for (int i = 1; i < m; i++) {
+            if (matrix[i][0] == 0) {
+                for (int j = 1; j < n; j++) {
+                    matrix[i][j] = 0;
+                }
+            }
+        }
+
+        // Set columns to zero
+        for (int j = 1; j < n; j++) {
+            if (matrix[0][j] == 0) {
+                for (int i = 1; i < m; i++) {
+                    matrix[i][j] = 0;
+                }
+            }
+        }
+
+        // Set first row and first column to zero if needed
+        if (firstRowHasZero) {
+            for (int j = 0; j < n; j++) {
+                matrix[0][j] = 0;
+            }
+        }
+
+        if (firstColHasZero) {
+            for (int i = 0; i < m; i++) {
+                matrix[i][0] = 0;
+            }
         }
     }
 };
