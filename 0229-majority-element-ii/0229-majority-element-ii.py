0021be2d6128abd782fc87e938280e785170815a
Debangshi Kundu <133627265+DebangshiKundu@users.noreply.github.com>
2025-05-24 00:02:50 +0530

    Time: 3 ms (92.45%), Space: 14.1 MB (40.94%) - LeetHub

--- 0229-majority-element-ii/0229-majority-element-ii.py
+++ 0229-majority-element-ii/0229-majority-element-ii.py
@@ -1,12 +1,37 @@
 class Solution(object):
     def majorityElement(self, nums):
-        hash={}
+        # hash={}
+        # res=[]
+        # for i in nums:
+        #     hash[i]=hash.get(i,0)+1
+        #     if hash[i]>len(nums)//3 and i not in res:
+        #         res.append(i)
+        # return res
+
+        c1,c2=0,0
+        n1,n2=None,None
         res=[]
-        for i in nums:
-            hash[i]=hash.get(i,0)+1
-            if hash[i]>len(nums)//3 and i not in res:
-                res.append(i)
+        for n in nums:
+            if n==n1: c1+=1
+            elif n==n2: c2+=1
+            elif c1==0:
+                n1=n
+                c1=1
+            elif c2==0:
+                n2=n
+                c2=1
+            else:
+                c1-=1
+                c2-=1
+        n=len(nums)
+        if nums.count(n1)>n//3:
+            res.append(n1)
+        if n2 is not None and n2!=n1 and nums.count(n2)>n//3:
+            res.append(n2)
         return res
+
+
+
         """
         :type nums: List[int]
         :rtype: List[int]
