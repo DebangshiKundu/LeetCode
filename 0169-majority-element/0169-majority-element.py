6c97c8fd8210c29c1ed1c1ac8bdc01b7851b57e9
Debangshi Kundu <133627265+DebangshiKundu@users.noreply.github.com>
2025-05-23 21:57:43 +0530

    Time: 2 ms (89.75%), Space: 13.8 MB (31.25%) - LeetHub

--- 0169-majority-element/0169-majority-element.py
+++ 0169-majority-element/0169-majority-element.py
@@ -5,10 +5,21 @@ class Solution(object):
         #     rec[i]=rec.get(i,0)+1
         #     if rec[i]>len(nums)//2: return i
 
-        nums.sort()
-        return nums[len(nums)//2]
+        # nums.sort()
+        # return nums[len(nums)//2]
+        no,c= None,0
+        for n in nums:
+            if no==n: c+=1
+            elif c==0:
+                no=n
+                c=1
+            else: c-=1
+        return no
         """
         :type nums: List[int]
         :rtype: int
         """
+
+
+
         
