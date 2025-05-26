aec2f9614583e69ef2a61386a66177a986807e4f
Debangshi Kundu <133627265+DebangshiKundu@users.noreply.github.com>
2025-05-27 00:42:17 +0530

    Time: 1110 ms (81.54%), Space: 22.7 MB (15.38%) - LeetHub

--- 0493-reverse-pairs/0493-reverse-pairs.py
+++ 0493-reverse-pairs/0493-reverse-pairs.py
@@ -1,14 +1,48 @@
 class Solution(object):
     def reversePairs(self, nums):
 
-        l = []
-        count = 0
+        # l = []
+        # count = 0
 
-        for num in reversed(nums):
-            count += bisect_left(l, num / 2.0)
-            insort(l, num)
+        # for num in reversed(nums):
+        #     count += bisect_left(l, num / 2.0)
+        #     insort(l, num)
 
-        return count
+        # return count
+
+        def merged_sort(left,right):
+            if left>=right: return 0
+
+            mid=(left+right)//2
+            count=merged_sort(left,mid)+merged_sort(mid+1,right)
+
+            j=mid+1
+            for i in range(left,mid+1):
+                while j<=right and nums[i]>2*nums[j]:
+                    j+=1
+                count+=j-(mid+1)
+            
+            i,j=left,mid+1
+            temp=[]
+            while i<=mid and j<=right:
+                if nums[i]<nums[j]:
+                    temp.append(nums[i])
+                    i+=1
+                else: 
+                    temp.append(nums[j])
+                    j+=1
+            while i<=mid:
+                temp.append(nums[i])
+                i+=1
+            while j<=right:
+                temp.append(nums[j])
+                j+=1
+            
+            nums[left:right+1]=temp
+            return count
+        return merged_sort(0,len(nums)-1)
+            
+
         """
         :type nums: List[int]
         :rtype: int
