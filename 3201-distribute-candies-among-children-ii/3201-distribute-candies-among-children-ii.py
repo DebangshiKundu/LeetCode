ad2557ead07ea844efe6d75f127ed957ffd3ba9c
Debangshi Kundu <133627265+DebangshiKundu@users.noreply.github.com>
2025-06-03 09:33:01 +0530

    Time: 38 ms (81.19%), Space: 18 MB (13.35%) - LeetHub

--- 3201-distribute-candies-among-children-ii/3201-distribute-candies-among-children-ii.py
+++ 3201-distribute-candies-among-children-ii/3201-distribute-candies-among-children-ii.py
@@ -1,9 +1,16 @@
 class Solution:
     def distributeCandies(self, n: int, limit: int) -> int:
-        res = 0
-        for i in range(min(limit, n) + 1):
-            if n - i <= 2 * limit:
-                res += min(n - i, limit) - max(0, n - i - limit) + 1
-        return res
+        def ncr(n,r):
+            if n<0 or r<0 or r>n: return 0
+            return math.comb(n,r)
+        
+        tot=ncr(n+2,2)
+        for i in range(3):
+            tot-=ncr(n-(limit+1)+2,2) if n-(limit+1)>=0 else 0
+        for i in range(3):
+            for j in range(i+1,3):
+                tot+=ncr(n-2 * (limit+1)+2,2) if n-2 * (limit+1)>=0 else 0
+        tot-=ncr(n-3 * (limit+1)+2,2) if n-3 * (limit+1)>=0 else 0
+        return tot
         
         
