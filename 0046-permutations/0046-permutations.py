030c1228c021fa90c133126d2d5f58a0ca4bc06a
Debangshi Kundu <133627265+DebangshiKundu@users.noreply.github.com>
2025-06-15 20:20:54 +0530

    Time: 0 ms (100%), Space: 18.1 MB (33.55%) - LeetHub

--- 0046-permutations/0046-permutations.py
+++ 0046-permutations/0046-permutations.py
@@ -3,13 +3,24 @@ class Solution:
         if len(nums) == 1 or 0:
             return [nums[:]]
         res=[]
-        for i in range(len(nums)):
-            n=nums.pop(0)
-            perm=self.permute(nums)
-            for p in perm:
-                p.append(n)
-                res.append(p)
-            nums.append(n)
+        # for i in range(len(nums)):
+        #     n=nums.pop(0)
+        #     perm=self.permute(nums)
+        #     for p in perm:
+        #         p.append(n)
+        #         res.append(p)
+        #     nums.append(n)
+        # return res
+
+        def make(start):
+            if start==len(nums):
+                res.append(nums[:])
+                return
+            for i in range(start,len(nums)):
+                nums[i],nums[start]=nums[start],nums[i]
+                make(start+1)
+                nums[i],nums[start]=nums[start],nums[i]
+        make(0)
         return res
 
             
