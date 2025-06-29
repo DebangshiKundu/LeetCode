46c40b67a6d9bf27e46629f1e09ad34fcba7f10f
Debangshi Kundu <133627265+DebangshiKundu@users.noreply.github.com>
2025-06-30 02:29:14 +0530

    Time: 138 ms (69.86%), Space: 80.1 MB (49.79%) - LeetHub

--- 0460-lfu-cache/0460-lfu-cache.py
+++ 0460-lfu-cache/0460-lfu-cache.py
@@ -1,38 +1,42 @@
 class LFUCache:
 
     def __init__(self, capacity: int):
-        self.cap = capacity
-        self.key2val = {}
-        self.key2freq = {}
-        self.freq2key = collections.defaultdict(collections.OrderedDict)
-        self.minf = 0
+        self.key2val={}
+        self.key2freq={}
+        self.cap=capacity
+        self.freq2key=defaultdict(OrderedDict)
+        self.minf=0
 
     def get(self, key: int) -> int:
         if key not in self.key2val:
             return -1
-        oldfreq = self.key2freq[key]
-        self.key2freq[key] = oldfreq + 1
-        self.freq2key[oldfreq].pop(key)
-        if not self.freq2key[oldfreq]:
-            del self.freq2key[oldfreq]
-        self.freq2key[oldfreq + 1][key] = 1
+        old=self.key2freq[key]
+        self.key2freq[key]=old+1
+        self.freq2key[old].pop(key)
+        if not self.freq2key[old]:
+            del self.freq2key[old]
+        self.freq2key[old+1][key]=1
         if self.minf not in self.freq2key:
-            self.minf += 1
+            self.minf+=1
         return self.key2val[key]
 
     def put(self, key: int, value: int) -> None:
-        if self.cap <= 0:
-            return
+        if self.cap<=0 : return
         if key in self.key2val:
             self.get(key)
-            self.key2val[key] = value
+            self.key2val[key]=value
             return
+        if self.cap==len(self.key2val):
+            delk,_=self.freq2key[self.minf].popitem(last=False)
+            del self.key2val[delk]
+            del self.key2freq[delk]
+        self.key2val[key]=value
+        self.key2freq[key]=1
+        self.freq2key[1][key]=1
+        self.minf=1
 
-        if len(self.key2val) == self.cap:
-            delkey, _ = self.freq2key[self.minf].popitem(last=False)
-            del self.key2val[delkey]
-            del self.key2freq[delkey]
-        self.key2val[key] = value
-        self.key2freq[key] = 1
-        self.freq2key[1][key] = 1
-        self.minf = 1
+
+# Your LFUCache object will be instantiated and called as such:
+# obj = LFUCache(capacity)
+# param_1 = obj.get(key)
+# obj.put(key,value)
