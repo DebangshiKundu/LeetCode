c83d1bebf9177f885972e45dc0e8385dcf96738a
Debangshi Kundu <133627265+DebangshiKundu@users.noreply.github.com>
2025-07-08 23:54:47 +0530

    Time: 0 ms (100%), Space: 18.5 MB (86.12%) - LeetHub

--- 0025-reverse-nodes-in-k-group/0025-reverse-nodes-in-k-group.py
+++ 0025-reverse-nodes-in-k-group/0025-reverse-nodes-in-k-group.py
@@ -5,53 +5,31 @@
 #         self.next = next
 class Solution:
     def reverseKGroup(self, head: Optional[ListNode], k: int) -> Optional[ListNode]:
-        # temp=head
-        # n=head
-        # while temp:
-        #     for _ in range(k-1):
-        #         temp=temp.next
-        #     if temp:
-        #         t=temp.next
-                
-        #         while n!=temp:
-        #             nn=n.next
-        #             nn.next=n
-        #             n=n.next
-        def has_k_nodes(node, k):
-            count = 0
-            while node and count < k:
-                node = node.next
-                count += 1
-            return count == k
-
-        # Helper to reverse k nodes
-        def reverse_k_nodes(head, k):
-            prev, curr = None, head
-            while k:
-                next_node = curr.next
+        def reverse(start: ListNode, k: int) -> Tuple[ListNode, ListNode]:
+            prev = None
+            curr = start
+            for _ in range(k):
+                nxt = curr.next
                 curr.next = prev
                 prev = curr
-                curr = next_node
-                k -= 1
-            return prev, head  # New head, new tail
+                curr = nxt
+            return prev, start  
 
         dummy = ListNode(0)
         dummy.next = head
         group_prev = dummy
 
-        while has_k_nodes(group_prev.next, k):
-            group_start = group_prev.next
-            group_end = group_start
-            for _ in range(k - 1):
-                group_end = group_end.next
+        while True:
+            kth = group_prev
+            for _ in range(k):
+                kth = kth.next
+                if not kth:
+                    return dummy.next
 
-            next_group = group_end.next
-            # Reverse the group
-            new_head, new_tail = reverse_k_nodes(group_start, k)
-            group_prev.next = new_head
-            new_tail.next = next_group
-
-            group_prev = new_tail
-
-        return dummy.next
-
+            group_next = kth.next
+            prev, tail = reverse(group_prev.next, k)
+            tail.next = group_next
+            temp = group_prev.next
+            group_prev.next = prev
+            group_prev = temp
+              
