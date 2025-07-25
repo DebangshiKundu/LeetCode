3357f381b7c106268b6c647e007d60a9e9ae6509
Debangshi Kundu <133627265+DebangshiKundu@users.noreply.github.com>
2025-07-25 13:01:00 +0530

    Time: 7 ms (97.55%), Space: 12.6 MB (82.88%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.py
+++ 0242-valid-anagram/0242-valid-anagram.py
@@ -5,17 +5,27 @@ class Solution(object):
         :type t: str
         :rtype: bool
         """
-        if len(s)!=len(t): return False
-        maps={}
-        for i in s:
-            if i not in maps:
-                maps[i]=0
-            maps[i]+=1
+        # if len(s)!=len(t): return False
+        # maps={}
+        # for i in s:
+        #     if i not in maps:
+        #         maps[i]=0
+        #     maps[i]+=1
 
-        for i in t:
-            if i not in maps:
-                return False
-            maps[i]-=1
-            if maps[i]==0:
-                del maps[i]
-        return len(maps)==0
+        # for i in t:
+        #     if i not in maps:
+        #         return False
+        #     maps[i]-=1
+        #     if maps[i]==0:
+        #         del maps[i]
+        # return len(maps)==0
+
+        q1=sorted(set(s))
+        q2=sorted(set(t))
+        s1=[s.count(i) for i in q1]
+        s2=[t.count(j) for j in q1]
+
+        print(sorted(set(s)))
+        print(s2)
+        return s1==s2 and q1==q2
+       
